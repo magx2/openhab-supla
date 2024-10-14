9eb0737bd71284a8ef5ae38a792857f00ee387a4
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-10-14 20:02:31 +0200

    use auth key as hex

---
+++ src/main/java/pl/grzeslowski/openhab/supla/internal/server/ByteArrayToHex.java
@@ -0,0 +1,24 @@
+package pl.grzeslowski.openhab.supla.internal.server;
+
+import lombok.experimental.UtilityClass;
+
+public interface ByteArrayToHex {
+     static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (byte b : bytes) {
+            String hex = String.format("%02X", b); // Convert byte to hex, ensuring two digits (e.g., "0A", "FF")
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+     static byte[] hexToBytes(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                    + Character.digit(hex.charAt(i+1), 16));
+        }
+        return data;
+    }
+}
