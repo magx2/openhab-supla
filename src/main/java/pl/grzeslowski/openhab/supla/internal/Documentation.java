473a1652228c5f42d5d0806c7d1953440ce2a124
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-10-19 11:59:54 +0200

    `Thing with Sub Devices`

--- src/main/java/pl/grzeslowski/openhab/supla/internal/Documentation.java
+++ src/main/java/pl/grzeslowski/openhab/supla/internal/Documentation.java
@@ -4,5 +4,5 @@ public interface Documentation {
     public static final String SSL_PROBLEM = "https://github.com/magx2/openhab-supla/tree/master#ssl-problem";
     public static final String DISABLED_ALGORITHMS_PROBLEM =
             "https://github.com/magx2/openhab-supla/tree/master#disabled-algorithms";
-    public static final String THING_BRIDGE = "https://todo.com"; // todo fill the proper FAQ link
+    public static final String THING_BRIDGE = "https://github.com/magx2/openhab-supla/tree/master#thing-with-sub-devices";
 }
