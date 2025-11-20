1ef1230608edd60213021f902c4f497d03bc60b0
Martin <martin.grzeslowski@gmail.com>
2025-11-20 14:02:22 +0100

    Adjust protocol test fixtures to valid record data

--- src/test/java/pl/grzeslowski/openhab/supla/internal/server/traits/DeviceChannelTest.java
+++ src/test/java/pl/grzeslowski/openhab/supla/internal/server/traits/DeviceChannelTest.java
@@ -2,7 +2,6 @@ package pl.grzeslowski.openhab.supla.internal.server.traits;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
 import static pl.grzeslowski.jsupla.protocol.api.ChannelFunction.SUPLA_CHANNELFNC_NONE;
 
 import org.junit.jupiter.api.Test;
@@ -21,7 +20,7 @@ class DeviceChannelTest {
 
     @Test
     void shouldBuildFromProtoA() {
-        byte[] value = new byte[] {0x01, 0x02};
+        byte[] value = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
         SuplaDeviceChannelA channelA = new SuplaDeviceChannelA((short) 5, 7, value);
 
         DeviceChannel deviceChannel = DeviceChannel.fromProto(channelA);
@@ -36,7 +35,7 @@ class DeviceChannelTest {
 
     @Test
     void shouldBuildFromProtoBWithChannelFunction() {
-        byte[] value = new byte[] {0x0A};
+        byte[] value = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x0A};
         SuplaDeviceChannelB channelB =
                 new SuplaDeviceChannelB((short) 10, 20, SUPLA_CHANNELFNC_NONE.getValue(), 0, value);
 
@@ -52,7 +51,7 @@ class DeviceChannelTest {
 
     @Test
     void shouldBuildFromProtoEWithSubDeviceId() {
-        byte[] value = new byte[] {0x0B, 0x0C};
+        byte[] value = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x0B, 0x0C};
         SuplaDeviceChannelE channelE = new SuplaDeviceChannelE(
                 (short) 11,
                 21,
@@ -80,7 +79,13 @@ class DeviceChannelTest {
 
     @Test
     void shouldAcceptHvacWithoutValue() {
-        HvacValue hvacValue = mock(HvacValue.class);
+        HvacValue hvacValue = new HvacValue(
+                true,
+                HvacValue.Mode.NOT_SET,
+                null,
+                null,
+                new HvacValue.Flags(
+                        false, false, false, false, false, false, false, false, false, false, false, false, false));
         DeviceChannel deviceChannel = new DeviceChannel(1, 2, SUPLA_CHANNELFNC_NONE, null, hvacValue, null);
 
         assertThat(deviceChannel.value()).isNull();
