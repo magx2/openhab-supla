2b329a7d97632f13dc96fd835d0943fda46986c1
Martin <martin.grzeslowski@gmail.com>
2026-01-11 14:53:51 +0100

    Remove stale DeviceChannel TODO

--- src/test/java/pl/grzeslowski/openhab/supla/internal/server/traits/DeviceChannelTest.java
+++ src/test/java/pl/grzeslowski/openhab/supla/internal/server/traits/DeviceChannelTest.java
@@ -6,11 +6,17 @@ import static pl.grzeslowski.jsupla.protocol.api.ChannelFunction.SUPLA_CHANNELFN
 
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import pl.grzeslowski.jsupla.protocol.api.BitFunction;
+import pl.grzeslowski.jsupla.protocol.api.ChannelFlag;
 import pl.grzeslowski.jsupla.protocol.api.ChannelType;
 import pl.grzeslowski.jsupla.protocol.api.HvacMode;
+import pl.grzeslowski.jsupla.protocol.api.channeltype.value.ActionTrigger;
 import pl.grzeslowski.jsupla.protocol.api.channeltype.value.HvacValue;
+import pl.grzeslowski.jsupla.protocol.api.structs.ActionTriggerProperties;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelA;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelC;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelD;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelE;
 
 @SuppressWarnings("deprecation")
@@ -98,6 +104,67 @@ class DeviceChannelTest {
         assertThat(deviceChannel.value()).containsExactly(value);
         assertThat(deviceChannel.hvacValue()).isNull();
         assertThat(deviceChannel.subDeviceId()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldBuildFromProtoCWithActionTrigger() {
+        int actionMask = ActionTrigger.Capabilities.TURN_ON.toMask();
+        ActionTriggerProperties actionTriggerProperties = new ActionTriggerProperties((short) 1, 0L);
+        SuplaDeviceChannelC channelC = new SuplaDeviceChannelC(
+                (short) 7,
+                ChannelType.SUPLA_CHANNELTYPE_ACTIONTRIGGER.getValue(),
+                null,
+                (long) actionMask,
+                SUPLA_CHANNELFNC_NONE.getValue(),
+                (int) ChannelFlag.SUPLA_CHANNEL_FLAG_WEEKLY_SCHEDULE.getValue(),
+                null,
+                actionTriggerProperties,
+                null);
+
+        DeviceChannel deviceChannel = DeviceChannel.fromProto(channelC);
+
+        assertThat(deviceChannel.number()).isEqualTo(7);
+        assertThat(deviceChannel.type()).isEqualTo(ChannelType.SUPLA_CHANNELTYPE_ACTIONTRIGGER);
+        assertThat(deviceChannel.flags()).contains(ChannelFlag.SUPLA_CHANNEL_FLAG_WEEKLY_SCHEDULE);
+        assertThat(deviceChannel.channelFunction()).isEqualTo(SUPLA_CHANNELFNC_NONE);
+        assertThat(deviceChannel.action())
+                .isNotNull()
+                .extracting(ActionTrigger::capabilities)
+                .isEqualTo(ActionTrigger.Capabilities.from(actionMask));
+        assertThat(deviceChannel.value()).isNull();
+        assertThat(deviceChannel.hvacValue()).isNull();
+    }
+
+    @Test
+    void shouldBuildFromProtoDWithFlagsAndValidity() {
+        byte[] value = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x0F, 0x10};
+        SuplaDeviceChannelD channelD = new SuplaDeviceChannelD(
+                (short) 12,
+                ChannelType.SUPLA_CHANNELTYPE_DIMMER.getValue(),
+                BitFunction.SUPLA_BIT_FUNC_LIGHTSWITCH.getValue(),
+                null,
+                SUPLA_CHANNELFNC_NONE.getValue(),
+                ChannelFlag.SUPLA_CHANNEL_FLAG_CHANNELSTATE.getValue(),
+                (short) 1,
+                120L,
+                value,
+                null,
+                null,
+                (short) 9);
+
+        DeviceChannel deviceChannel = DeviceChannel.fromProto(channelD);
+
+        assertThat(deviceChannel.number()).isEqualTo(12);
+        assertThat(deviceChannel.type()).isEqualTo(ChannelType.SUPLA_CHANNELTYPE_DIMMER);
+        assertThat(deviceChannel.offline()).isTrue();
+        assertThat(deviceChannel.flags()).contains(ChannelFlag.SUPLA_CHANNEL_FLAG_CHANNELSTATE);
+        assertThat(deviceChannel.functions()).contains(BitFunction.SUPLA_BIT_FUNC_LIGHTSWITCH);
+        assertThat(deviceChannel.valueValidityTimeSec()).isEqualTo(120L);
+        assertThat(deviceChannel.defaultIcon()).isEqualTo(9);
+        assertThat(deviceChannel.channelFunction()).isEqualTo(SUPLA_CHANNELFNC_NONE);
+        assertThat(deviceChannel.value()).containsExactly(value);
+        assertThat(deviceChannel.action()).isNull();
+        assertThat(deviceChannel.hvacValue()).isNull();
     }
 
     @Test
